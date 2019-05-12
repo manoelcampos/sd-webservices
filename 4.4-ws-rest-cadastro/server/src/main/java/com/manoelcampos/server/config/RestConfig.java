5dbe3704798dea406955fc2874b0e3c3baad8ee8
Manoel Campos <manoelcampos@gmail.com>
2019-05-12 16:22:19 -0300

    Commit inicial depois de mover os projetos de Web Services para um repositório próprio, e antes de adicioná-lo como um submódulo no repositório sistemas-distribuidos

---
+++ 4.4-ws-rest-cadastro/server/src/main/java/com/manoelcampos/server/config/RestConfig.java
@@ -0,0 +1,14 @@
+package com.manoelcampos.server.config;
+
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ * @author manoelcampos
+ */
+@javax.ws.rs.ApplicationPath("api")
+public class RestConfig extends Application {
+
+
+    
+}
