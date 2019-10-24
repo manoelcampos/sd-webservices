d043492590d2b12607c7f3263a92ada453590349
Manoel Campos <manoelcampos@gmail.com>
2019-10-24 14:28:49 -0300

    Remove projeto 4.7-ws-rest-thorntail-appserver

--- 4.7-ws-rest-thorntail-appserver/src/main/resources/META-INF/dados.sql
+++
@@ -1,4 +0,0 @@
-insert into Cidade (nome, uf) values ("Palmas", "TO"), ("Araguaina", "TO"), ("Gurupi", "TO"), ("Porto Nacional", "TO");
-insert into Usuario (cpf, nome, cidade_id) values ("11111111111", "Manoel", 1), ("22222222222", "Maria", 1), ("33333333333", "Raimundo", 2);
-insert into Cliente (nome) values ("Jose"), ("Pedro"), ("Rosana");
-insert into Endereco (logradouro, cidade_id, cliente_id) values ("Rua 1", 1, 1), ("Rua 1", 1, 2), ("Rua 2", 2, 3);
